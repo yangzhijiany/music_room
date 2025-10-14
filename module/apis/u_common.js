4f6e7d1679657fb1b5a434f852eee9c7cba3dd58
yangzhijiany <yangzhijiany2021@163.com>
2025-10-14 00:30:20 -0500

    修复Cookie传递问题：优先使用全局配置的cookie

--- module/apis/u_common.js
+++ module/apis/u_common.js
@@ -3,12 +3,15 @@ const config = require('../config');
 const userInfoConfig = require('../../config/user-info');
 
 module.exports = ({ options = {}, method = 'get' }) => {
+	// 优先使用全局配置的cookie，如果没有则使用本地配置
+	const cookie = global.userInfo?.cookie || userInfoConfig.cookie;
+	
 	let opts = Object.assign(options, config.commonParams, {
 		headers: {
 			referer: 'https://y.qq.com/portal/player.html',
 			host: 'u.y.qq.com',
 			'content-type': 'application/x-www-form-urlencoded',
-			'cookie': userInfoConfig.cookie,
+			'cookie': cookie,
 		},
 	});
 	console.log('https://u.y.qq.com/cgi-bin/musicu.fcg', { opts })
